5db4fb75eded606a8309f3a2b1fff5d02f835924
Egor <mregor978@gmail.com>
2020-02-29 21:44:50 +0300

    make GanttCanvas.js component, base architecture

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 import { App } from "./App";
 
-import "./normalize.css";
+import "./styles/normalize.css";
 import "./index.sass";
 
 ReactDOM.render(<App />, document.getElementById("root"));
