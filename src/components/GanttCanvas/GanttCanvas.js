163f3cf8e2739c549884bfda6120ad665ad0a9fc
Egor <mregor978@gmail.com>
2020-03-09 10:07:59 +0300

    add onResize and draw functions

--- src/components/GanttCanvas/GanttCanvas.js
+++ src/components/GanttCanvas/GanttCanvas.js
@@ -1,19 +1,60 @@
-import React, { useRef } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
+
+import { fills } from "./fills";
 
 import classes from "./GanttCanvas.module.sass";
 
 export const GanttCanvas = () => {
   const { gantt, gantt__title, gantt__chart } = classes;
-  const canvasRef = useRef(null);
+  const { RED } = fills;
 
-  function createCanvas(canvas) {
-    const context2D = canvas.getContext("2d");
-  }
+  const canvasRef = useRef(null);
+  const ganttRef = useRef(null);
+
+  const drawRect = useCallback(
+    chart => {
+      chart.fillStyle = RED;
+      chart.fillRect(0, 200, 100, 12);
+    },
+    [RED]
+  );
+
+  const drawChart = useCallback(
+    chart => {
+      chart.scale(1, 1);
+      chart.translate(0, 0);
+      drawRect(chart);
+    },
+    [drawRect]
+  );
+
+  useEffect(() => {
+    const chart = canvasRef.current.getContext("2d");
+
+    const onResize = () => {
+      const PIXEL_RATIO = window.devicePixelRatio;
+      chart.width = chart.offsetWidth * PIXEL_RATIO;
+      chart.height = chart.offsetHeight * PIXEL_RATIO;
+      chart.setTransform(PIXEL_RATIO, 0, 0, PIXEL_RATIO, 0, 0);
+      drawChart(chart);
+    };
+
+    window.addEventListener("resize", onResize);
+    onResize(chart);
+
+    return window.removeEventListener("resize", onResize);
+  }, [drawChart]);
 
   return (
-    <div className={gantt}>
+    <div className={gantt} ref={ganttRef}>
       <div className={gantt__title}>Canvas</div>
-      <canvas ref={canvasRef} id="gantt-canvas" className={gantt__chart}>
+      <canvas
+        ref={canvasRef}
+        id="gantt-canvas"
+        className={gantt__chart}
+        width={1500}
+        height={400}
+      >
         Данный виджет не поддерживается вашим браузером
       </canvas>
     </div>
---
+++ src/components/GanttCanvas/fills.js
@@ -0,0 +1,4 @@
+export const fills = {
+  RED: "#FF5248",
+  WHITE: "#D9E2E6"
+};
