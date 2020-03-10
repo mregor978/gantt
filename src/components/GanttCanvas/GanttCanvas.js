2b3941d776d36fd3db8c968be31cf5256636340d
Egor <mregor978@gmail.com>
2020-03-10 16:59:31 +0300

    add scroll function

--- src/components/GanttCanvas/GanttCanvas.js
+++ src/components/GanttCanvas/GanttCanvas.js
@@ -1,11 +1,7 @@
 import React, { useCallback, useEffect, useRef } from "react";
-
 import moment from "moment";
-
 import { fills } from "./fills";
-
 import { ganttDataCreator } from "../../utils/ganttData";
-
 import classes from "./GanttCanvas.module.sass";
 
 export const GanttCanvas = () => {
@@ -16,6 +12,19 @@ export const GanttCanvas = () => {
   const ganttRef = useRef(null);
 
   const chartWidth = 5430;
+
+  let dragActive = false;
+  let startX = 0;
+  let chartTranslateX = 0;
+
+  const activateDrag = e => {
+    dragActive = true;
+    startX = e.pageX;
+  };
+
+  const disableDrag = () => {
+    dragActive = false;
+  };
 
   const getDateXCoord = useCallback(
     date => {
@@ -39,29 +48,47 @@ export const GanttCanvas = () => {
   const { ganttData } = new ganttDataCreator(5);
 
   const drawChart = useCallback(
-    chart => {
+    (translateX = 0) => {
+      const canvas = canvasRef.current;
+      const chart = canvas.getContext("2d");
+      chart.clearRect(0, 0, canvas.width, canvas.height);
       chart.scale(1, 1);
-      chart.translate(0, 0);
+      chart.translate(translateX, 0);
       drawRect(chart);
     },
     [drawRect]
   );
 
   useEffect(() => {
-    const chart = canvasRef.current.getContext("2d");
-
+    const canvas = canvasRef.current;
+    const chart = canvas.getContext("2d");
     const onResize = () => {
       const PIXEL_RATIO = window.devicePixelRatio;
-      chart.width = chart.offsetWidth * PIXEL_RATIO;
-      chart.height = chart.offsetHeight * PIXEL_RATIO;
+      canvas.width = canvas.offsetWidth * PIXEL_RATIO;
+      canvas.height = canvas.offsetHeight * PIXEL_RATIO;
       chart.setTransform(PIXEL_RATIO, 0, 0, PIXEL_RATIO, 0, 0);
-      drawChart(chart);
+      drawChart();
+    };
+
+    const onDrag = e => {
+      if (dragActive) {
+        const delta = e.pageX - startX;
+        startX = e.pageX;
+        drawChart(delta);
+      }
     };
 
     window.addEventListener("resize", onResize);
+    window.addEventListener("mousemove", onDrag);
+    window.addEventListener("mouseup", disableDrag);
+
     onResize(chart);
 
-    return window.removeEventListener("resize", onResize);
+    return () => {
+      window.removeEventListener("resize", onResize);
+      window.removeEventListener("mousemove", onDrag);
+      window.removeEventListener("mouseup", disableDrag);
+    };
   }, [drawChart]);
 
   return (
@@ -73,6 +100,7 @@ export const GanttCanvas = () => {
         className={gantt__chart}
         width={1500}
         height={400}
+        onMouseDown={activateDrag}
       >
         Данный виджет не поддерживается вашим браузером
       </canvas>
